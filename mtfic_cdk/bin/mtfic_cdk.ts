1b0f64bfd67e0d4078bd38abf88be71f867a7554
Supun <supun@blackswan-technologies.com>
2023-01-06 18:56:50 +0000

    testing ECS

--- mtfic_cdk/bin/mtfic_cdk.ts
+++ mtfic_cdk/bin/mtfic_cdk.ts
@@ -1,9 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import { Construct } from 'constructs';
 
-import { S3BucketStack } from '../lib/s3-bucket-test-stack';
+// import { S3BucketStack } from '../lib/s3-bucket-test-stack';
 
 const app = new cdk.App();
 
-// Creating an S3 bucket stack
-new S3BucketStack(app, 'mtficBucketStack');
+// // Creating an S3 bucket stack
+// new S3BucketStack(app, 'mtficBucketStack');
 
+
+
+declare const cluster: ecs.Cluster;
+const loadBalancedFargateService = new ecs_patterns.ApplicationMultipleTargetGroupsFargateService(app, 'Service', {
+  cluster,
+  memoryLimitMiB: 1024,
+  cpu: 512,
+  taskImageOptions: {
+    image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+  },
+  targetGroups: [
+    {
+      containerPort: 80,
+    },
+    {
+      containerPort: 90,
+      pathPattern: '/rabbits',
+      priority: 10,
+    },
+  ],
+});

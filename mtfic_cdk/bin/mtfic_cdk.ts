3231b367f0cbb524b0098dd99c255077d18d804d
Supun <supun@blackswan-technologies.com>
2023-01-08 09:18:18 +0000

    Finalizing

--- mtfic_cdk/bin/mtfic_cdk.ts
+++ mtfic_cdk/bin/mtfic_cdk.ts
@@ -1,13 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 
-// import { S3BucketStack } from '../lib/s3-bucket-test-stack';
-
 import { MtficEcsStack } from '../lib/ecs-stack';
 
 const app = new cdk.App();
-
-// // Creating an S3 bucket stack
-// new S3BucketStack(app, 'mtficBucketStack');
 
 // new MtficEcsStack(app, 'mtficECSStack');
 
